84e495aa7bffb1f1f63f506607cc2f53f2c5687f
Alex Grytsai <a.grytsai1987@gmail.com>
2024-06-04 12:42:23 +0300

    set up in settings.py: django-bootstrap4, django-crispy-forms and crispy-bootstrap4

--- mytraining/settings.py
+++ mytraining/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "bootstrap4",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "app",
 ]
 
@@ -133,6 +136,10 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
