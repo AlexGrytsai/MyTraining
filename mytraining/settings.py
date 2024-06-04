d2f533647e373ca0e8d94c6889e38be337bfc58f
Alex Grytsai <a.grytsai1987@gmail.com>
2024-06-04 18:04:07 +0300

    created User class and added AUTH_USER_MODEL in settings.py

--- mytraining/settings.py
+++ mytraining/settings.py
@@ -150,7 +150,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-# AUTH_USER_MODEL = "users.WorkerUser"
+AUTH_USER_MODEL = "app.User"
 
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
